36e91c1cfc13271b6673b945b326c1cfe154d00d
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-06-15 20:53:07 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,7 @@ radio.setGroup(23)
 let x = 0
 let y = 0
 let trim = 0
-let canDrive = true
-let autoPilot = false
+let mode = "manual"
 
 type IRC = {
     l: DigitalPin,
@@ -26,64 +25,49 @@ function setMotors(leftSpeed: number, rightSpeed: number) {
 }
 
 radio.onReceivedValue(function (name, value) {
-    if (name === "x") {
-        x = value
-    } else if (name === "y") {
-        y = value
-    } else if (name === "drive") {
-        canDrive = value === 1
-    } else if (name === "trim") {
+    if (name === "trim") {
         trim = value
-    } else if (name === "autopilot") {
-        autoPilot = true
+    } else if (name === "mode") {
+        mode = value === 1 ? "auto" : "manual"
+    }
+})
+
+radio.onReceivedString(function (receivedString: string) {
+    let parts = receivedString.split(",")
+    if (parts.length == 2) {
+        x = parseInt(parts[0])
+        y = parseInt(parts[1])
     }
 })
 
 basic.forever(function () {
-    if (canDrive && autoPilot === false) {
+    if (mode === "manual") {
         let leftSpeed = x - y - trim
         let rightSpeed = x + y + trim
 
-        if (leftSpeed > 255) leftSpeed = 255
-        if (leftSpeed < -255) leftSpeed = -255
-        if (rightSpeed > 255) rightSpeed = 255
-        if (rightSpeed < -255) rightSpeed = -255
+        leftSpeed = Math.max(-255, Math.min(255, leftSpeed))
+        rightSpeed = Math.max(-255, Math.min(255, rightSpeed))
 
         setMotors(leftSpeed, rightSpeed)
+    } else if (mode === "auto") {
+        let dataL = pins.digitalReadPin(IR.l)
+        let dataC = pins.digitalReadPin(IR.c)
+        let dataR = pins.digitalReadPin(IR.r)
 
-    } else if (autoPilot === true) {
-
-        let dataR = pins.digitalReadPin(IR.r);
-        console.log(dataR);
-
-        let dataC: number;
-        dataC = pins.digitalReadPin(IR.c);
-        console.log(dataC);
-
-        let dataL: number;
-        dataL = pins.digitalReadPin(IR.l);
-        console.log(dataL);
-
-        if (dataC === 0) {
-
-            setMotors(150, 150);
-        } else if (dataL === 0) {
-
-            setMotors(100, 150);
-        } else if (dataR === 0) {
-
-            setMotors(150, 100);
-        } else { setMotors(0, 0);
+        if (dataC === 1) {
+            setMotors(150, 150)
+        } else if (dataL === 1) {
+            setMotors(150, 100)
+        } else if (dataR === 1) {
+            setMotors(100, 150)
+        } else {
+            setMotors(0, 0)
         }
-
-
 
     } else {
         PCAmotor.MotorStop(PCAmotor.Motors.M1)
         PCAmotor.MotorStop(PCAmotor.Motors.M4)
     }
-
-    //basic.showNumber(y)
 
     basic.pause(25)
 })
