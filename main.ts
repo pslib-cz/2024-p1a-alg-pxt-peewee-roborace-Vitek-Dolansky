4313b2c56f225677e89f8e0512ca063b424d08ba
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-06-18 13:33:30 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,8 +17,8 @@ pins.setPull(IR.r, PinPullMode.PullNone);
 
 
 function forward() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 150)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, -150)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 100)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -100)
 }
 function left() {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, 100)
@@ -32,24 +32,37 @@ function stop() {
     PCAmotor.MotorStopAll()
 }
 
-
+let last_side = "";
 basic.forever(function () {
     let dataL = pins.digitalReadPin(IR.l)
     let dataR = pins.digitalReadPin(IR.r)
     let dataC = pins.digitalReadPin(IR.c)
+
     console.log([dataL, dataC, dataR]);
+    console.log(last_side)
     if (dataC === 1) {
         forward()
-    } else if (dataL === 1) {
+        console.log("forward")
+    } else if (dataL === 1 && dataR === 0) {
         left()
-    } else if (dataR === 1) {
+        last_side = "l";
+        console.log("left")
+    } else if (dataR === 1 && dataL === 0) {
         right()
+        last_side = "r";
+        console.log("right")
     } else {
-        stop()
+        
+        if (last_side === "r") {
+            right()
+        }
+        else if (last_side === "l") {
+            left()
+        }
     }
 
 
-    input.onButtonPressed(Button.AB, function() {
+    input.onButtonPressed(Button.AB, function () {
         basic.showNumber(dataC)
     })
 
